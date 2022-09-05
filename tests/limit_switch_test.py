b217dfe7f31be10016a9b46233994b40687efaae
Kevin Yu <kevinyu567@gmail.com>
2022-09-05 11:50:15 -0700

    Add limit switch test

---
+++ tests/limit_switch_test.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from constants import FORWARD_LS_PIN, BACKWARD_LS_PIN
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup([FORWARD_LS_PIN, BACKWARD_LS_PIN], GPIO.IN)
+
+
+# Logs a detected edge on the given channel.
+def report_edge(channel):
+    print(f"Rising edge detected on channel {channel}.")
+
+
+if __name__ == '__main__':
+    GPIO.add_event_detect(FORWARD_LS_PIN, GPIO.RISING, callback=report_edge)
+    GPIO.add_event_detect(BACKWARD_LS_PIN, GPIO.RISING, callback=report_edge)
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        GPIO.cleanup()
