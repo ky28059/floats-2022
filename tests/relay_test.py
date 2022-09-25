629db9fc6143440626fa7a26aeb649623bb14df3
Kevin Yu <kevinyu567@gmail.com>
2022-09-25 14:07:00 -0700

    Run both relays during test

--- tests/relay_test.py
+++ tests/relay_test.py
@@ -1,15 +1,15 @@
 import time
 import RPi.GPIO as GPIO
-from constants import FM_RELAY_PIN
+from constants import FM_RELAY_PIN, LED_RELAY_PIN
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(FM_RELAY_PIN, GPIO.OUT)
+GPIO.setup([FM_RELAY_PIN, LED_RELAY_PIN], GPIO.OUT)
 
 if __name__ == '__main__':
-    print(f"Starting relay on pin {FM_RELAY_PIN}.")
-    GPIO.output(FM_RELAY_PIN, GPIO.HIGH)
+    print(f"Starting relay on pins {FM_RELAY_PIN} and {LED_RELAY_PIN}.")
+    GPIO.output([FM_RELAY_PIN, LED_RELAY_PIN], GPIO.HIGH)
     time.sleep(5)
 
     print("Stopping relay.")
-    GPIO.output(FM_RELAY_PIN, GPIO.LOW)
+    GPIO.output([FM_RELAY_PIN, LED_RELAY_PIN], GPIO.LOW)
     GPIO.cleanup()
