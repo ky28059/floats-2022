5d9e08e5c2401921acdaeb2a1f4ce177f1c241f0
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-10-08 10:18:39 -0700

    Replace `GPIO.wait_for_edge` with events

--- tests/limit_switch_motor_test.py
+++ tests/limit_switch_motor_test.py
@@ -4,6 +4,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 import RPi.GPIO as GPIO
+from multiprocessing import Event
 from constants import TALON_PIN, FORWARD_LS_PIN, BACKWARD_LS_PIN, CYCLE_TIME, PULSE_FREQUENCY, MAX_MS, MID_MS
 
 GPIO.setmode(GPIO.BCM)
@@ -13,6 +14,9 @@ GPIO.setup([FORWARD_LS_PIN, BACKWARD_LS_PIN], GPIO.IN, pull_up_down=GPIO.PUD_DOW
 print(f"Running with frequency={PULSE_FREQUENCY}Hz, duty cycle min={1.0 / CYCLE_TIME}, max={2.0 / CYCLE_TIME}")
 
 talon = GPIO.PWM(TALON_PIN, PULSE_FREQUENCY)
+
+forward_ls = Event()
+backward_ls = Event()
 
 
 # Converts more standard [-1.0, 1.0] percent output values to talon duty cycle percentages [0.0, 100.0].
@@ -28,7 +32,16 @@ def run_talon(p: float) -> None:
     talon.ChangeDutyCycle(convert_duty_cycle(p))
 
 
+# Waits for a rising edge on the given limit switch.
+def wait_for_edge(ls: Event):
+    ls.clear()
+    ls.wait()
+
+
 if __name__ == '__main__':
+    GPIO.add_event_detect(FORWARD_LS_PIN, GPIO.RISING, callback=forward_ls.set)
+    GPIO.add_event_detect(BACKWARD_LS_PIN, GPIO.RISING, callback=backward_ls.set)
+
     print("Starting TalonSRX PWM signal.")
     talon.start(convert_duty_cycle(0))
 
@@ -36,11 +49,11 @@ if __name__ == '__main__':
         while True:
             print(f"Running Talon forwards until rising edge on {FORWARD_LS_PIN}.")
             run_talon(0.5)
-            GPIO.wait_for_edge(FORWARD_LS_PIN, GPIO.RISING)
+            wait_for_edge(forward_ls)
 
             print(f"Running Talon backwards until rising edge on {BACKWARD_LS_PIN}.")
             run_talon(-0.5)
-            GPIO.wait_for_edge(BACKWARD_LS_PIN, GPIO.RISING)
+            wait_for_edge(backward_ls)
 
     except KeyboardInterrupt:
         print("Test finished, cleaning up.")
