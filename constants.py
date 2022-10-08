b4ee95a549ac6a828c6d071a4a791ab3dc3ec48e
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-10-08 11:48:58 -0700

    Test hardware PWM

--- constants.py
+++ constants.py
@@ -10,3 +10,4 @@ PULSE_FREQUENCY = 1000.0 / CYCLE_TIME  # Hz (up to 100Hz)
 MIN_MS = 0.85  # 1ms nominal, min 0.6ms
 MAX_MS = 1.95  # 2ms nominal, max 2.4ms
 MID_MS = (MIN_MS + MAX_MS) / 2.0
+PWM_RANGE = 255
