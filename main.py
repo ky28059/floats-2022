8b37760ccd0876b08969c8347148bea3e511dadd
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-10-10 17:23:17 -0700

    Run radio in foreground

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from modules.radio import radio
 from modules.schedule import during_school, is_passing, schedule_process
 
 hatch_process = Process(target=hatch, args=(during_school, is_passing), daemon=True)
-radio_process = Process(target=radio, args=(during_school, is_passing), daemon=True)
 
 if __name__ == '__main__':
     schedule_process.start()
     hatch_process.start()
-    radio_process.start()
 
     try:
-        while True:
-            pass
+        radio(during_school, is_passing)
     except KeyboardInterrupt:
         hatch_process.join()
-        radio_process.join()
